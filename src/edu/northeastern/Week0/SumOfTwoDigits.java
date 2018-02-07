ebfaa9ae0b5685c8553384867b6033f47a708e15
saurabhsingh13no <singh.saurab@husky.neu.edu>
2018-02-06 23:42:03 -0500

    pushed my first code

---
+++ src/edu/northeastern/Week0/SumOfTwoDigits.java
@@ -0,0 +1,14 @@
+package edu.northeastern.Week0;
+import java.util.Scanner;
+
+public class SumOfTwoDigits {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 2 integers : ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println("Sum of two numbers : "+(a+b));
+    }
+}
